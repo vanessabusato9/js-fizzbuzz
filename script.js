ca07b278c0d80fae3f078b7205a25cc51d11d9e5
vanessa <vane.busato9@gmail.com>
2025-11-01 16:34:00 +0100

    tolto numero quando stampa fizz/buzz/fizzbuzz

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ Stampare fizzBuzz con multipli di 3 e 5*/
 
 for (let i = 1; i <= 100; i += 1) {
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log(i + " fizzBuzz");
+        console.log("FizzBuzz");
+        //console.log(i + " FizzBuzz") In questo caso viene stampato il numero e FizzBuzz
     } else if (i % 3 === 0) {
-        console.log(i + " fizz");
+        console.log("Fizz");
     } else if (i % 5 === 0) {
-        console.log(i + " buzz");
+        console.log("Buzz");
     } else {
         console.log(i);
     }
